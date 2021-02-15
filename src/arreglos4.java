eb57e8062fb734776ad27c27a22fd00be5fa62c4
pedrolo <pedrolopez7411@gmail.com>
2021-02-15 20:00:56 -0300

    denuevo

--- src/arreglos4.java
+++ src/arreglos4.java
@@ -1,9 +1,20 @@
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
 public class arreglos4 {
-
+/*Hacer un programa que dado un arreglo de enteros de tamaño 10 que se encuentra
+precargado, solicite al usuario un numero entero y lo agregue al principio del arreglo (posición
+0). Para ello tendrá que realizar un corrimiento a derecha (se pierde el último valor del arreglo) y
+colocar el numero en el arreglo en la posición indicada.*/
+	public static int MAX=10;
+	public static int MAXVALOR;
+	public static int MINVALOR;
+	public static int[] enteros = new int[MAX];
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//cargar arreglo
+		//pedir un numero entero
+		//agregar al principio del arreglo
+		//hacer corrimiento a derecha 
 	}
 
 }
